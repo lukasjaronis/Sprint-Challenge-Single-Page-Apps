4b767acb305ac7a3ad250fc643a44f68b3aae9bd
lukasjaronis <jaronisluke13@gmail.com>
2019-11-15 11:16:14 -0500

    CharacterCard complete of what I want the card to look like and what to take in

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,11 +1,32 @@
 import React from "react";
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({
+  name,
+  gender,
+  species,
+  status,
+  imageURL
+}) {
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia className={classes.media} alt={name} image={imageURL} />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Species: {species}
+            Status: {status}
+            Gender: {gender}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 }
