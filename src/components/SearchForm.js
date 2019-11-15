c3bf02999da1de5d3e5387471f8662a2b2ee65ed
lukasjaronis <jaronisluke13@gmail.com>
2019-11-15 11:41:08 -0500

    useEffect and handleChanges set up for search

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,6 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import {Axios as Requester} from "axios";
+import { Link } from "react-router-dom";
+import CharacterCard from "./CharacterCard";
 
 export default function SearchForm() {
+  const [data, updateData] = useState( [] );
+  const [search, setSearch] = useState( [] );
+
+
+  useEffect( () => {
+
+  })
  
   return (
     <section className="search-form">
